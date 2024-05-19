9f484b36e1b5aa0d4bd4c792058d85abbe8ec7c6
Carlos Villanueva <carlos2villanueva@gmail.com>
2024-05-19 17:56:55 -0400

    Initial Commit

---
+++ TEST APP/TEST_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  TEST_APPApp.swift
+//  TEST APP
+//
+//  Created by Carlos Villanueva on 5/19/24.
+//
+
+import SwiftUI
+
+@main
+struct TEST_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
